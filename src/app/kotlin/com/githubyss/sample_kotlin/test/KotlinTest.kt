dff2cccb93e92db85220a1d450b75e88213c075c
ace <hotyss@hotmail.com>
2022-05-20 15:59:00 +0800

    完善时间延迟样例

--- src/app/kotlin/com/githubyss/sample_kotlin/test/KotlinTest.kt
+++ src/app/kotlin/com/githubyss/sample_kotlin/test/KotlinTest.kt
@@ -1,6 +1,7 @@
 package com.githubyss.sample_kotlin.test
 
 import com.githubyss.sample_kotlin.test.thread.thread
+import com.githubyss.sample_kotlin.test.time_delay.timeDelay
 
 
 fun main(args: Array<String>) {
@@ -12,6 +13,8 @@ fun main(args: Array<String>) {
 
     // TestThread.thread()
     thread()
+
+    timeDelay()
 
     // TestSynchronized.synchronize()
 }
--- src/app/kotlin/com/githubyss/sample_kotlin/test/time_delay/TestTimeDelay.kt
+++ src/app/kotlin/com/githubyss/sample_kotlin/test/time_delay/TestTimeDelay.kt
@@ -1,5 +1,10 @@
 package com.githubyss.sample_kotlin.test.time_delay
 
+import android.os.Handler
+import java.util.*
+import kotlin.concurrent.thread
+import kotlin.concurrent.timerTask
+
 
 /**
  * TestTimeDelay
@@ -8,16 +13,71 @@ package com.githubyss.sample_kotlin.test.time_delay
  * @github githubyss
  * @createdTime 2022/05/20 14:13:23
  */
+
+private var delayInMillis: Long = 3000
+
 fun timeDelay() {
+    println("调测延迟")
+    println()
+
     delayThread()
+    delayTimerTask()
+    // delayHandler()
 }
 
-/**
- * 通过 Thread 实现延时
- *
- * @param
- * @return
- */
 private fun delayThread() {
+    object : Thread() {
+        override fun run() {
+            println("object : Thread() {} >> delay start!")
+            sleep(delayInMillis)
+            println("object : Thread() {} >> delay finish!")
+        }
+    }.start()
 
+    Thread {
+        run {
+            println("Thread { run {}} >> delay start!")
+            Thread.sleep(delayInMillis)
+            println("Thread { run {}} >> delay finish!")
+        }
+    }.start()
+
+    Thread {
+        println("Thread {} >> delay start!")
+        Thread.sleep(delayInMillis)
+        println("Thread {} >> delay finish!")
+    }.start()
+
+    thread {
+        println("thread {} >> delay start!")
+        Thread.sleep(delayInMillis)
+        println("thread {} >> delay finish!")
+    }
+
+    println()
+}
+
+private fun delayTimerTask() {
+    println("object : TimerTask() {} >> delay start!")
+    Timer().schedule(object : TimerTask() {
+        override fun run() {
+            println("object : TimerTask() {} >> delay finish!")
+        }
+    }, delayInMillis)
+
+    println("timerTask {} >> delay start!")
+    Timer().schedule(timerTask {
+        println("timerTask {} >> delay finish!")
+    }, delayInMillis)
+
+    println()
+}
+
+private fun delayHandler() {
+    println("postDelayed {} >> delay start!")
+    Handler().postDelayed(Runnable {
+        println("postDelayed {} >> delay finish!")
+    }, delayInMillis)
+
+    println()
 }
